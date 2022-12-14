2c83790c3800423631e4e6809d18ce8e331a908e
nckrkllkc <n.karakullukcu@hotmail.com>
2022-12-14 05:43:00 +0300

    Nckrkllkc

---
+++ PlusMinus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    static class PlusMinus
+    {
+        public static void plusMinus(List<int> arr)
+        {
+            // Format içerisinde kullanılan ilk 0 virgülden sonraki ilk değeri temsil ediyor.
+            // : işaret formatlamanın nasıl yapılacağını tanımlamak için kullanılır.
+            // Devamındaki tanım zaten format tanımı :)
+
+            // Değişken tanımlayarak da çözebilirsiniz. Ben gereksiz yere bellek israfı olmaması için bu şekilde yaptım.
+            // Otorite değilim. Doğrusu budur demiyorum. Daha sonra kullanma ihtiyacım yoksa değişken tanımlayıp, bellek
+            // israfına girmemenin doğru yöntem olduğunu öğrendim. O yüzden bu şekilde yazdım :)
+
+            Console.WriteLine(String.Format("{0:0.000000}", Convert.ToDecimal(arr.Where(n => n > 0).ToList().Count) / Convert.ToDecimal(arr.Count)));
+            Console.WriteLine(String.Format("{0:0.000000}", Convert.ToDecimal(arr.Where(n => n < 0).ToList().Count) / Convert.ToDecimal(arr.Count)));
+            Console.WriteLine(String.Format("{0:0.000000}", Convert.ToDecimal(arr.Where(n => n == 0).ToList().Count) / Convert.ToDecimal(arr.Count)));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,27 +4,8 @@
     {
         static void Main(string[] args)
         {
-            List<List<int>> arr = new List<List<int>>();
-            List<int> ar1 = new List<int>();
-            ar1.Add(11);
-            ar1.Add(2);
-            ar1.Add(4);
-            List<int> ar2 = new List<int>();
-            ar2.Add(4);
-            ar2.Add(5);
-            ar2.Add(6);
-
-            List<int> ar3 = new List<int>();
-            ar3.Add(10);
-            ar3.Add(8);
-            ar3.Add(-12);
-            
-            arr.Add(ar1);
-            arr.Add(ar2);
-            arr.Add(ar3);
-
-
-            DiagonalDifference.diagonalDifference(arr);
+            List<int> arr = new() { -4, 3, -9, 0, 4, 1 };
+            PlusMinus.plusMinus(arr);
         }
     }
 }
