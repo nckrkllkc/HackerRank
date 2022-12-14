fc3bca570ed01618c27c89bcbe9b6d03e9658318
nckrkllkc <n.karakullukcu@hotmail.com>
2022-12-14 06:07:19 +0300

    Nckrkllkc

--- PlusMinus.cs
+++ PlusMinus.cs
@@ -11,16 +11,24 @@ namespace HackerRank
         public static void plusMinus(List<int> arr)
         {
             // Format içerisinde kullanılan ilk 0 virgülden sonraki ilk değeri temsil ediyor.
-            // : işaret formatlamanın nasıl yapılacağını tanımlamak için kullanılır.
+            // : işaret formatlamanın nasıl yapılacağını tanımlamak için kullanılır. Son hane yuvarlanır.
             // Devamındaki tanım zaten format tanımı :)
 
-            // Değişken tanımlayarak da çözebilirsiniz. Ben gereksiz yere bellek israfı olmaması için bu şekilde yaptım.
-            // Otorite değilim. Doğrusu budur demiyorum. Daha sonra kullanma ihtiyacım yoksa değişken tanımlayıp, bellek
-            // israfına girmemenin doğru yöntem olduğunu öğrendim. O yüzden bu şekilde yazdım :)
+            // arr.Where(n => n > 0).ToList().Count / Convert.ToDecimal(arr.Count)
+            // veya
+            // arr.Where(n => n > 0).ToList().Count / arrCount
 
-            Console.WriteLine(String.Format("{0:0.000000}", Convert.ToDecimal(arr.Where(n => n > 0).ToList().Count) / Convert.ToDecimal(arr.Count)));
-            Console.WriteLine(String.Format("{0:0.000000}", Convert.ToDecimal(arr.Where(n => n < 0).ToList().Count) / Convert.ToDecimal(arr.Count)));
-            Console.WriteLine(String.Format("{0:0.000000}", Convert.ToDecimal(arr.Where(n => n == 0).ToList().Count) / Convert.ToDecimal(arr.Count)));
+            // Değişken tanımlayarak da çözebilirsiniz. Daha sonra kullanma ihtiyacım yoksa değişken tanımlayıp, bellek
+            // israfına girmemenin doğru yöntem olduğunu okumuştum bir yerde. Değişken tanımlamadan kullanılan yöntemde convert
+            // etme ve listenin eleman sayısını alma işlemini 3 defa yapmış olduk. Hangisinden feragat etmek istersin
+            // deseler bellek derim. Ram candır ama işlemci de öyle. O yüzden değişken tanımlamasıyla fazla işlemden kaçınmış
+            // olduk :)
+
+            decimal arrCount = Convert.ToDecimal(arr.Count);
+            Console.WriteLine(String.Format("{0:0.000000}", arr.Where(n => n > 0).ToList().Count / arrCount));
+            Console.WriteLine(String.Format("{0:0.000000}", arr.Where(n => n < 0).ToList().Count / arrCount));
+            Console.WriteLine(String.Format("{0:0.000000}", arr.Where(n => n == 0).ToList().Count / arrCount));
+
         }
     }
 }
